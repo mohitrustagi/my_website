a8902770321ccc06d51a1db98b5f9bdcb2d6211c
Mohit Rustagi <mohit@Mohits-MacBook-Pro.local>
2025-05-27 17:06:15 +0530

    fix errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Mohit Rustagi',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    description: '',
     siteUrl: 'https://mohitrustagi.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
